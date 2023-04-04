027423ee6feeb227371d04cab2f714119ce1b527
bordon <1078464063@qq.com>
2023-04-04 17:42:04 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
 
 // https://astro.build/config
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import { remarkReadingTime } from "./src/utils/all";
+
 export default defineConfig({
-  site: 'https://lemon-coral.vercel.app/',
-  integrations: [vue(), tailwind(), mdx(), sitemap()]
+  site: "https://lemon-coral.vercel.app",
+  experimental: {
+    contentCollections: true,
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
+    extendDefaultPlugins: true,
+  },
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
