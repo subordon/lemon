d7db1c84c72bd94b1b8557e4016ae855af7f2071
bordon <1078464063@qq.com>
2023-08-31 17:33:06 +0800

    feat: test build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 // https://astro.build/config
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -10,9 +9,6 @@ import { remarkReadingTime } from "./src/utils/all";
 
 export default defineConfig({
   site: "https://lemon-coral.vercel.app",
-  experimental: {
-    // assets: true,
-  },
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [],
